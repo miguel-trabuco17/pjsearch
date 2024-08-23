dfa7f0ba00c5635cb1ba100755cff1df34371921
Miguel Trabuco <migueltrabuco17@gmail.com>
2024-08-22 22:47:14 -0300

    Create get input value and submit button logic

--- src/components/MaskInput.jsx
+++ src/components/MaskInput.jsx
@@ -1,7 +1,25 @@
 import styles from '../styles/components/MaskInput.module.css';
 import MaskedInput from 'react-input-mask';
+import { useState } from 'react';
 
 export default function MaskInput() {
+
+	const [cnpj, setCnpj] = useState('');
+	const [formatedCnpj, setFormatedCnpj] = useState('');
+	const [buttonState, setButtonState] = useState(false);
+
+	const handleInputChange = (event) => {
+		const inputValue = event.target.value;
+		setCnpj(inputValue);
+		const formatedInputValue = inputValue.replace(/\D/g, '');
+		setFormatedCnpj(formatedInputValue);
+		if (formatedInputValue.length == 14) {
+			setButtonState(true);
+		} else {
+			setButtonState(false);
+		}
+	};
+
 	return (
 		<div className={styles.submitArea}>
 			<div className={styles.inputBox}>
@@ -9,8 +27,10 @@ export default function MaskInput() {
 					className={styles.input}
 					placeholder='00.000.000/0000-00'
 					mask='99.999.999/9999-99'
+					value={cnpj}
+					onChange={handleInputChange}
 				/>
-				<button className={styles.submitButton}>Pesquisar</button>
+				<button className={styles.submitButton} disabled={!buttonState}>Pesquisar</button>
 			</div>
 			<button className={styles.mobileSubmitButton}>Pesquisar</button>
 		</div>
